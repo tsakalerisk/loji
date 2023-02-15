06c1c302b97ee656349e16e3a12c64486d2d0ff7
Konstantinos Tsakaleris <13333629+tsakalerisk@users.noreply.github.com>
2023-02-15 15:58:39 +0200

    Added log in

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,12 +5,25 @@
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![delete_cookies])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn delete_cookies(window: tauri::Window) {
+    window
+        .with_webview(|webview| {
+            #[cfg(windows)]
+            unsafe {
+                use webview2_com::Microsoft::Web::WebView2::Win32::ICoreWebView2_10;
+                use windows::core::Interface;
+
+                let core_webview2_10: ICoreWebView2_10 =
+                    webview.controller().CoreWebView2().unwrap().cast().unwrap();
+                let cookie_manager = core_webview2_10.CookieManager().unwrap();
+                cookie_manager.DeleteAllCookies().unwrap();
+            }
+        })
+        .expect("failed");
 }
--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import logo from './loji_logo.svg';
 import './App.css';
-// import { invoke } from '@tauri-apps/api'
+import { invoke } from '@tauri-apps/api'
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { useState } from 'react';
+import firebaseConfig from './firebaseConfig.json';
+import { CSSTransition } from 'react-transition-group';
 
+
+// eslint-disable-next-line no-unused-vars
+const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account'
+});
+const auth = getAuth();
 
 function App() {
-  // invoke('greet', { name: 'World' })
-  // // `invoke` returns a Promise
-  // .then((response) => console.log(response));
+  const [user, setUser] = useState(null);
+
+  getAuth().onAuthStateChanged(user => {
+    setUser(user)
+  });
+
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Menu>
-
+        <button>Start</button>
+        <button>Daily Challenge</button>
+        <button>Credits</button>
       </Menu>
+      <LoginButton user={user} />
     </div>
   );
 }
@@ -26,12 +45,52 @@ function Header() {
   );
 }
 
-function Menu() {
+function Menu(props) {
   return (
     <div className='Menu'>
-      <button>Start</button>
-      <button>Daily Challenge</button>
-      <button>Credits</button>
+      {props.children}
+    </div>
+  );
+}
+
+function LoginButton(props) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className='login-area'>
+      <button className='login-button' onClick={() => {
+        if (!props.user) {
+          signInWithPopup(auth, provider).catch((error) => {
+            console.log(error);
+          });
+        } else {
+          setOpen(!open);
+        }
+      }}>
+        {props.user && <img src={props.user.photoURL} alt="User's profile" className='profile-picture' />}
+        <span className='login-text'>{(props.user && props.user.displayName) || "Log in"}</span>
+      </button>
+      <CSSTransition in={open} classNames='dropdown-animation' timeout={250} unmountOnExit>
+        <DropDown callback={() => setOpen(false)}/>
+      </CSSTransition>
+    </div>
+  );
+}
+
+function DropDown(props) {
+  return (
+    <div className='dropdown'>
+      <button>Settings</button>
+      <button className='signout-button' onClick={() => {
+        signOut(getAuth()).then(() =>
+          invoke('delete_cookies')
+        ).catch(error =>
+          console.log(error)
+        );
+        props.callback();
+      }}>
+        Sign out
+      </button>
     </div>
   );
 }

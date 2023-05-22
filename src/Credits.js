a0d6d73c556972957d2963e9def7dcf3eb3cf2a1
Konstantinos Tsakaleris <13333629+tsakalerisk@users.noreply.github.com>
2023-05-22 12:44:40 +0300

    Update credits

--- src/Credits.js
+++ src/Credits.js
@@ -3,14 +3,30 @@ import KostasProfile from './kostas.jpg'
 
 const Credits = () => {
     return <div className="credits-container">
+        <div className='credits'>
+            <h1 style={{textAlign: 'center'}}>
+                Riddles created by
+                    Lazaros Koemtzopoulos,
+                    Evangelos Tzotzis,
+                    and Alexandros Spitalas
+            </h1>
+        </div>
         <div className="credits">
             <img src={KostasProfile} alt=""/>
             <div className="bio-container">
-                <h1>Made by Kostas Tsakaleris</h1>
+                <h1>Developed by Kostas Tsakaleris</h1>
                 <div className="bio">
-                    I am an Applied Informatics major with a love for software development.
-                    <br/>
-                    You can find more projects on my GitHub <a href='https://github.com/tsakalerisk'>here</a>.
+                    <p>
+                        I am an Applied Informatics major 
+                        with a focus on software development.
+                    </p>
+                    <p>
+                        I mostly work with C, C++, and Java on the back-end,
+                        and vanilla Javascript and React on the front-end.
+                    </p>
+                    <p>
+                        You can find more projects on my GitHub <a href='https://github.com/tsakalerisk'>here</a>.
+                    </p>
                 </div>
             </div>
         </div>
